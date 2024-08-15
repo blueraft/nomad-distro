488d3f985610ba858a3c61da2549aaece38e6fc5
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-08-15 10:38:13 +0200

    Upgrade package in develop deployments

--- scripts/generate_python_dependencies.sh
+++ scripts/generate_python_dependencies.sh
@@ -9,6 +9,7 @@ cd $project_dir
 
 
 uv pip compile --universal -p 3.11 --annotation-style=line \
+    --upgrade-package nomad-lab \
     --extra=plugins \
     --output-file=requirements.txt \
     pyproject.toml
